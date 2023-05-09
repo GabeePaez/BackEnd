9116f78ab2e8e543aeb3de87eb80f61d397136e9
Gabee <116602883+GabeePaez@users.noreply.github.com>
2023-05-09 01:08:28 -0300

    docker 11

--- src/main/java/com/gab/gab/Controller/PersonaController.java
+++ src/main/java/com/gab/gab/Controller/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://frontend-2b9dd.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = "https://frontend-2b9dd.web.app")
 public class PersonaController {
 
     @Autowired
--- src/main/java/com/gab/gab/Security/Controller/AuthController.java
+++ src/main/java/com/gab/gab/Security/Controller/AuthController.java
@@ -34,8 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = {"https://frontend-2b9dd.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = "https://frontend-2b9dd.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
